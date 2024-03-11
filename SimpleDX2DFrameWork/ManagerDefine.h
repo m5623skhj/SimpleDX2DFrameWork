5ca111ffba89409d678e1f642e7120b285f2a5a1
m5623skhj <m5623skhj@gmail.com>
2024-03-12 00:13:13 +0900

    * fmod 헤더 include * SoundManager 구현 ** 연관 상수들 추가

--- SimpleDX2DFrameWork/ManagerDefine.h
+++ SimpleDX2DFrameWork/ManagerDefine.h
@@ -12,3 +12,16 @@ public: \
 		static NonCopyableClass instance; \
 		return instance; \
 	}
+
+#define NON_COPYABLE_WITH_NON_DEFAULT_DESTRUCTOR(NonCopyableClass) \
+private: \
+	NonCopyableClass() = default; \
+	~NonCopyableClass(); \
+	NonCopyableClass(const NonCopyableClass& other) = delete; \
+	NonCopyableClass& operator=(const NonCopyableClass& other) = delete; \
+public: \
+	static NonCopyableClass& GetInst() \
+	{ \
+		static NonCopyableClass instance; \
+		return instance; \
+	}
--- SimpleDX2DFrameWork/SoundManager.h
+++ SimpleDX2DFrameWork/SoundManager.h
@@ -1,11 +1,39 @@
 #pragma once
+#include "fmod.h"
 #include "ManagerDefine.h"
+#include "Define.h"
+#include <string_view>
+#include <unordered_map>
 
 class SoundManager
 {
-	NON_COPYABLE(SoundManager);
+	NON_COPYABLE_WITH_NON_DEFAULT_DESTRUCTOR(SoundManager);
 
 public:
 	void Initialize();
 
+private:
+	bool LoadSoundData();
+
+	bool LoadBackgroundMusicFile(std::string_view soundFolderPath, std::string_view soundFilePath);
+	bool LoadSoundEffectFile(std::string_view soundFolderPath, std::string_view soundFilePath);
+
+private:
+	void ReleaseSound(std::string_view releaseSound);
+
+public:
+	void UpdateSound();
+
+public:
+	bool StartBackgroundMusic(std::string_view targetBackgroundMusic);
+	bool StartSoundEffect(std::string_view targetSoundEffect);
+	void StopMusic(int stopSoundChannel);
+
+private:
+	FMOD_SYSTEM* fmodSystem;
+
+	std::unordered_map<std::string_view, FMOD_SOUND*> soundMap;
+	FMOD_CHANNEL* channelArray[soundChannelMax];
+
+	int channelPointer = soundChannelMin;
 };
